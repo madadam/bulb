2cd7a9379d4efbaf222353903a29dc43479336dd
Adam Cigánek <adam.ciganek@gmail.com>
2011-09-20 22:45:51 +0200

    Adds stlyling

--- public/app.js
+++ public/app.js
@@ -1,10 +1,48 @@
 (function($) {
   $(document).ready(function() {
-    $("a[data-remote]").click(function() {
-      // handle the click
+    var prepareItem = function(item) {
+      item.find(".text").editable("/", {type: "textarea"});
+      item.draggable();
+    }
 
+    $("#ideas li").each(function() { prepareItem($(this)); });
 
-      return false;
+    // Add new idea.
+    $("#new-idea").click(function() {
+      var list = $("#ideas");
+      var item = $("<li>");
+      var text = $("<div>").addClass("text").text("Put your idea here.");
+
+      item.append(text);
+      item.hide();
+      prepareItem(item);
+
+      list.append(item);
+
+      item.slideDown(function() {
+        text.click();
+        text.find("textarea").select();
+        $.scrollTo(text);
+      });
+    });
+
+    // Trash.
+    $("#trash").droppable({
+      tolerance: "touch",
+      over: function() { $(this).addClass("active"); },
+      out: function() { $(this).removeClass("active"); },
+
+      drop: function(event, ui) {
+        var idea  = ui.draggable;
+        var trash = $(this);
+
+        // TODO: actally trash the idea.
+
+        idea.fadeOut(200, function() {
+          idea.remove();
+          trash.removeClass("active");
+        });
+      }
     });
   });
 })(jQuery);
